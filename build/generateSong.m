b6fbcc9bb6490e7f224966c7a872769579073247
Veikko Sariola <veikko@andrew.cmu.edu>
2017-10-08 23:26:35 +0300

    Add dropping unnecessary patterns.

--- build/generateSong.m
+++ build/generateSong.m
@@ -1,5 +1,28 @@
 f = fopen('../data/styge');d = fread(f,'uint8=>uint8');fclose(f);
 song = loadSoundBoxBin(d);
+for i = 1:length(song.songData)
+    p = song.songData{i}{2};
+    c = song.songData{i}{3}; 
+    up = unique(p);
+    usedPatterns = up(up > 0);
+    l = 1:length(c);
+    drop = ismember(l,usedPatterns);
+    finalc = c(drop);
+    mapping = zeros(1,length(c));
+    mapping(l(drop))=1:length(finalc);
+    zeromapping = [0 mapping];
+    song.songData{i}{2} = zeromapping(p+1);
+    song.songData{i}{3} = finalc;
+    if (any(mapping == 0)) 
+       fprintf('On channel %d, found unused patterns: ',i);
+       for j = 1:length(mapping)
+           if (mapping(j) == 0)
+               fprintf('%d ',j)
+           end
+       end
+       fprintf('\n');
+    end
+end
 song.endPattern = song.endPattern + 4;
 exportMat(song,'../src/loadsong.m');
 rehash;
