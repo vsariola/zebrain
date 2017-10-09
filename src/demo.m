c7d0f4b0f54607de99808086aa75cd790ac2071d
Veikko Sariola <veikko.sariola@tut.fi>
2017-10-09 16:42:48 +0300

    Change the effect into delaunay torus.

--- src/demo.m
+++ src/demo.m
@@ -11,12 +11,34 @@ function demo
     a = audioplayer(s/32768,44100);
     play(a);
     
-    l=-9:.1:9;[x,tmp]=ndgrid(l);
-    while isplaying(a)        
-        v = y(:,floor((a.CurrentSample-1)/4+1));
-        h=x+x'*1i;
-        for F=0:4;for K=0:3;F=F+1./(h-exp(1i*(K+v(1))));end;h=h-3./F;end;
-        image(abs(F));
+    u = rand(6e2,1)*2*pi;
+    v = rand(6e2,1)*2*pi;
+    uu = linspace(0,2*pi,10)';
+    vv = uu*0;
+    u = [u;uu;uu;vv;vv+2*pi];
+    v = [v;vv;vv+2*pi;uu;uu];
+    D = 10;
+    A = 3.5;
+    K = 5;
+    W = 3;
+        
+    tri = delaunay(u,v);
+       
+    while isplaying(a)      
+        currentSample = a.CurrentSample;
+        i = currentSample/song.rowLen;        
+        i = i/10;
+        x = (cos(u)*A+K*sin(W*v)+D).*cos(v);
+        y = (cos(u)*A+K*sin(W*v)+D).*sin(v);
+        z = sin(u)*A;
+        trisurf(tri,x,y,z,'FaceColor','interp','LineWidth',2);                  
+        daspect([1 1 1]);        
+        camproj('perspective')
+        camva(75);
+        camtarget([5 5 1]);
+        camup([1 0 1]);
+        campos([(D+K*sin(W*i))*cos(i),(D+K*sin(W*i))*sin(i),0]);
+        colormap bone 
         drawnow;
     end
     
