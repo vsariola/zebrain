0e9d2be554f125a212c358617b4ac25c1c55bd36
vsariola <veikko.sariola@gmail.com>
2019-11-06 12:19:03 +0200

    Remove hard coded path from encode script.

--- encode.bat
+++ encode.bat
@@ -1,1 +1,1 @@
-d:\portable\ffmpeg\bin\ffmpeg -i src/output/video.avi -i src/output/audio.wav -c:v libx264 -c:a aac -movflags +faststart src/output/combined.mp4
+ffmpeg -i src/output/video.avi -i src/output/audio.wav -c:v libx264 -c:a aac -movflags +faststart src/output/combined.mp4
